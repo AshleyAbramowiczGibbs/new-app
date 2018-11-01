e2f49eba20364da8555e6d2c18891d8dfd1b0ae9
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-10-31 19:09:41 -0500

    Create news app using API

---
+++ app/controllers/api/articles_controller.rb
@@ -0,0 +1,7 @@
+class Api::ArticlesController < ApplicationController
+
+  def index
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=#{ENV["API_KEY"]}")
+    render json: response.parse["articles"]
+  end
+end
